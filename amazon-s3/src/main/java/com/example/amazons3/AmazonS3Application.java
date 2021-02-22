ca65dbd2227287451cc17f94d458229551274c80
KarthikSJ97 <karthik.s.jhingade@gmail.com>
2021-02-22 12:53:59 +0530

    [Initial setup] | feature | Added a simple spring boot application

---
+++ amazon-s3/src/main/java/com/example/amazons3/AmazonS3Application.java
@@ -0,0 +1,13 @@
+package com.example.amazons3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonS3Application.class, args);
+	}
+
+}
