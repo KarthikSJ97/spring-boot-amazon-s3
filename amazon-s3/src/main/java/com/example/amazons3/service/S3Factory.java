84c3278cd0639033cf23408a3b60b7d344e28f7e
KarthikSJ97 <karthik.s.jhingade@gmail.com>
2022-02-16 15:06:58 +0530

    [S3Factory] | refactor | Deleting locally stored file and using try-with-resource for auto stream closing

--- amazon-s3/src/main/java/com/example/amazons3/service/S3Factory.java
+++ amazon-s3/src/main/java/com/example/amazons3/service/S3Factory.java
@@ -4,18 +4,22 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.Bucket;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import lombok.extern.log4j.Log4j2;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Service
+@Log4j2
 public class S3Factory {
 
     @Autowired
@@ -31,21 +35,31 @@ public class S3Factory {
         return amazonS3Client.listBuckets();
     }
 
-    public void uploadFile(String name,byte[] content)  {
+    public void uploadFile(String name,byte[] content) throws IOException {
 
-//      TODO: Try to avoid storing it in file before uploading it to S3
         File file = new File("src/main/resources/"+name);
-        FileOutputStream fileOutputStream;
-        try {
-            fileOutputStream = new FileOutputStream(file);
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+        try(fileOutputStream) {
             fileOutputStream.write(content);
             amazonS3Client.putObject(defaultBucketName, defaultBaseFolder+"/"+file.getName(), file);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error("Some error occurred while uploading file to S3...");
+        } finally {
+            deleteLocallyStoredFile(file);
         }
-        file.delete();
+    }
+
+    @Async
+    private void deleteLocallyStoredFile(File file) {
+        CompletableFuture.runAsync(() -> {
+            boolean fileDeleted = false;
+            try {
+                fileDeleted = Files.deleteIfExists(file.toPath());
+            } catch (IOException e) {
+                log.error("Error occurred while deleting file: {}", file.getPath());
+            }
+            log.info("File deletion status: {}", fileDeleted);
+        });
     }
 
     public byte[] getFile(String key) {
